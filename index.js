416771ab80262e2fd9b32a46410aa7ecb6400539
MonicaChambi <monica.chambi@unmsm.edu.pe>
2022-10-27 16:34:21 -0500

    freeze commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run(usuario) {
   });
  
  
-  const PORT = 3001;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
